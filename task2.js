4e28b9f1d768eff831d51b711f7e091d9bc17129
godkate <bogodul883@gmail.com>
2022-10-23 13:21:41 -0400

    Added second script

--- task2.js
+++ task2.js
@@ -1,6 +1,12 @@
 //Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів
 
 console.log('start task 2')
-
+let dollar = 10;
+const uah = 27;
+let money = "";
+for (dollar; dollar <= 100; dollar += 10) {
+    money = `${dollar} доларів - це ${dollar * uah} гривень`;
+    console.log(money)
+}
 
 console.log('end task 2')

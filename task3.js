e206de4e1e3a951a276daa459034335078043d9d
godkate <bogodul883@gmail.com>
2022-10-23 22:19:06 -0400

    Added third script

--- task3.js
+++ task3.js
@@ -1,1 +1,11 @@
 //Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N
+
+console.log('start task 3')
+const integer = 36;
+for (firstInteger = 1; firstInteger <=100; firstInteger++) {
+    let squareNumber = firstInteger*firstInteger;
+    if (squareNumber <= integer) {
+        console.log(firstInteger);
+    }
+}
+console.log('end task 3')

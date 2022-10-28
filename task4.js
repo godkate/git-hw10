c3436c235ca5b1b417d45ce33d908378e495341a
godkate <bogodul883@gmail.com>
2022-10-28 12:13:47 -0400

    Added final changes

--- task4.js
+++ task4.js
@@ -1,12 +1,20 @@
 //Дане ціле число. З'ясувати, чи є воно простим
 // (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
 console.log('start task 4')
-const x = 5;
-if (x % 2 === 0) {
-    console.log(`Число ${x} не є простим`);
-} else {
-    console.log(`Число ${x} є простим`);
+let x = 3;
+let y = 2;
+function isPrime (num){
+    if (num === 1) {
+        return 'Число не є простим';
+    } else if (num === 2 || num === 3) {
+        return 'Число є простим';
+    } else {
+        for (y; y <= num; y++) {
+            if (num % y === 0) {
+                return 'Число не є простим';
+            }
+        }
+    } return 'Число є простим';
 }
+console.log(x, isPrime(x));
 console.log('end task 4')
-
-//До чого тут цикли?

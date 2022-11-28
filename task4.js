64b2101447843e03b0225760a84701f9c3f59f5b
godkate <bogodul883@gmail.com>
2022-11-27 22:06:19 -0500

    fixed mistake

--- task4.js
+++ task4.js
@@ -1,20 +1,19 @@
 //Дане ціле число. З'ясувати, чи є воно простим
 // (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
 console.log('start task 4')
-let x = 3;
-let y = 2;
+
 function isPrime (num){
     if (num === 1) {
         return 'Число не є простим';
     } else if (num === 2 || num === 3) {
         return 'Число є простим';
     } else {
-        for (y; y <= num; y++) {
+        for (let y = 2; y < num; y++) {
             if (num % y === 0) {
                 return 'Число не є простим';
             }
         }
     } return 'Число є простим';
 }
-console.log(x, isPrime(x));
+console.log(isPrime(89));
 console.log('end task 4')
